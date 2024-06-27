2d2880d9cd08dbd32c5398ff75a9605045ded8f6
Ritesh <riteshpatel246@gmail.com>
2024-06-27 19:16:23 +0530

    update

--- _components/Header.jsx
+++ _components/Header.jsx
@@ -6,6 +6,7 @@ import { FaBarsStaggered } from "react-icons/fa6";
 import { MdAccountCircle } from "react-icons/md";
 import { BiBookmarkHeart } from "react-icons/bi";
 import { Button } from "@/components/ui/button";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,25 +19,27 @@ const Header = () => {
     <div>
       {/* Top section with contact number */}
       <div className="bg-blue-600 text-white p-1">
-        <div className="container mx-auto text-center">
-          <div className="flex items-center justify-center space-x-2">
-            <FaWhatsapp />
-            <p>+987456123</p>
-            <p>(Free Delivery on above 499/-)</p>
+        <div className="container mx-auto">
+          <div className="flex justify-between">
+            <p className="flex ">
+              {" "}
+              <FaWhatsapp /> +987456123
+            </p>
+            <p>Free Delivery on above 499₹ 🎉</p>
           </div>
         </div>
       </div>
 
       {/* Main header section */}
-      <div className="bg-white shadow-md">
+      <div className="bg-black text-white shadow-md">
         <div className="container mx-auto flex justify-between items-center p-3">
-          <div className="text-2xl font-bold">E-commerce</div>
+          <div className="text-sm md:text-md lg:text-xl font-bold">E-com</div>
           <div className="hidden md:flex items-center space-x-4">
             <div className="relative">
               <input
                 type="text"
                 placeholder="Search"
-                className="border border-gray-300 rounded-full py-1 px-3"
+                className="border border-gray-300 text-black rounded-full py-1 px-3"
               />
             </div>
           </div>
@@ -45,7 +48,7 @@ const Header = () => {
               <input
                 type="text"
                 placeholder="Search"
-                className="border border-gray-300 rounded-full py-1 px-3 w-full"
+                className="border border-gray-300 text-black rounded-full py-1 px-3 w-full"
               />
             </div>
             <a href="">
@@ -68,24 +71,28 @@ const Header = () => {
       </div>
 
       {/* Mobile menu */}
-      {isOpen && (
-        <div className="md:hidden bg-white shadow-md">
-          <nav className="flex flex-col items-center space-y-4 py-4">
-            <a href="/" className="text-gray-700 hover:text-gray-900">
-              Home
-            </a>
-            <a href="/about" className="text-gray-700 hover:text-gray-900">
-              Category
-            </a>
-            <a href="/about" className="text-gray-700 hover:text-gray-900">
-              Category
-            </a>
-            <Button className="border rounded-full bg-blue-500 py-1 text-white">
-              Login
-            </Button>
-          </nav>
-        </div>
-      )}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0, x: "+100%" }}
+            animate={{ opacity: 10, x: 0 }}
+            exit={{ opacity: 0, x: "-100%" }}
+            className="md:hidden bg-transparent shadow-md w-3/4"
+          >
+            <nav className="flex flex-col items-center  space-y-4 py-4">
+              <a href="/" className="text-gray-700 hover:text-gray-500">
+                Home
+              </a>
+              <a href="/" className="text-gray-700 hover:text-gray-500">
+                Category
+              </a>
+              <Button className="border rounded-full bg-black py-1 text-white hover:text-gray-300">
+                Login
+              </Button>
+            </nav>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
