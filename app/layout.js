0055d016aa5380f99b78edc7d5d54527a7aaa6b0
DevSammyKad <DevSammyKad@gmail.com>
2024-07-18 16:11:22 +0530

    Admin Dashboard Added

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "next-themes";
-import Footer from "@/_components/footer";
-import { AOSInit } from "@/components/ui/aos-init";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from 'next-themes';
+import Footer from '@/_components/footer';
+// import { AOSInit } from '@/components/ui/aos-init';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "E-Comm",
-  description: "Welcome to shopping application",
+  title: 'E-Comm',
+  description: 'Welcome to shopping application',
 };
 
 export default function RootLayout({ children }) {
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light">
-          <AOSInit />
+          {/* <AOSInit /> */}
           {children}
           <Footer />
         </ThemeProvider>
--- components/ui/input.jsx
+++ components/ui/input.jsx
@@ -1,58 +1,19 @@
-"use client";
-import React, { useState, forwardRef } from "react";
-import { cn } from "../../lib/utils";
-import { useMotionTemplate, useMotionValue, motion } from "framer-motion";
+import * as React from "react"
 
-const Input = forwardRef((props, ref) => {
-  const { className, type, ...rest } = props;
-  const radius = 100; // change this to increase the radius of the hover effect
-  const [visible, setVisible] = useState(false);
+import { cn } from "@/lib/utils"
 
-  let mouseX = useMotionValue(0);
-  let mouseY = useMotionValue(0);
-
-  function handleMouseMove({ currentTarget, clientX, clientY }) {
-    let { left, top } = currentTarget.getBoundingClientRect();
-
-    mouseX.set(clientX - left);
-    mouseY.set(clientY - top);
-  }
-
+const Input = React.forwardRef(({ className, type, ...props }, ref) => {
   return (
-    <motion.div
-      style={{
-        background: useMotionTemplate`
-          radial-gradient(
-            ${
-              visible ? radius + "px" : "0px"
-            } circle at ${mouseX}px ${mouseY}px,
-            var(--blue-500),
-            transparent 80%
-          )
-        `,
-      }}
-      onMouseMove={handleMouseMove}
-      onMouseEnter={() => setVisible(true)}
-      onMouseLeave={() => setVisible(false)}
-      className="p-[2px] rounded-lg transition duration-300 group/input"
-    >
-      <input
-        type={type}
-        className={cn(
-          `flex h-10 w-full border-none bg-gray-50 text-black shadow-input rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent 
-          file:text-sm file:font-medium placeholder:text-neutral-400  
-          focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400
-          disabled:cursor-not-allowed disabled:opacity-50
-          group-hover/input:shadow-none transition duration-400`,
-          className
-        )}
-        ref={ref}
-        {...rest}
-      />
-    </motion.div>
+    (<input
+      type={type}
+      className={cn(
+        "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        className
+      )}
+      ref={ref}
+      {...props} />)
   );
-});
+})
+Input.displayName = "Input"
 
-Input.displayName = "Input";
-
-export { Input };
+export { Input }
